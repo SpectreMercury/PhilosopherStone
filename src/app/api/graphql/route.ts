41b155da77da8438293e937873d8fa3bd140492e
lee920217 <francislee920217@gmail.com>
2023-12-26 08:50:55 +0800

    update maxduration for hobby

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,7 +2,7 @@ import { startSporeServerNextHandler } from 'spore-graphql/next';
 import { predefinedSporeConfigs } from '@spore-sdk/core';
 
 export const fetchCache = 'force-no-store';
-export const maxDuration = 300;
+export const maxDuration = 10;
 
 const handler = startSporeServerNextHandler(predefinedSporeConfigs.Aggron4);
 export { handler as GET, handler as POST };
