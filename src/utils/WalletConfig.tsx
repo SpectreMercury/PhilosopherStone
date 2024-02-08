7352286e46263de023c8cfd2e28c2e9a19fd37a9
lee920217 <francislee920217@gmail.com>
2024-02-08 22:22:56 +0800

    receive record could be found when uer get gift via url

--- src/utils/WalletConfig.tsx
+++ src/utils/WalletConfig.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import { WalletConfig } from "@/types/Wallet";
 
-const WalletConfigData: WalletConfig[] = [
+let WalletConfigData: WalletConfig[] = [
   {
     name: 'Metamask',
     recommended: false,
     logo: <Image src='/svg/metamask-icon.svg' alt='Metamask' width={24} height={24} />,
   },
-  // {
-  //   name: 'JoyID',
-  //   recommended: false,
-  //   logo: <Image src='/svg/joyid-icon.svg' alt='JoyID' width={24} height={24} />,
-  // },
 ];
+
+WalletConfigData = process.env.NODE_ENV === 'development' ? [...WalletConfigData, {
+  name: 'JoyID',
+  recommended: false,
+  logo: <Image src='/svg/joyid-icon.svg' alt='JoyID' width={24} height={24} />,
+},] : WalletConfigData
+
 
 export default WalletConfigData;
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import { forkSporeConfig, predefinedSporeConfigs, setSporeConfig, SporeConfig } from '@spore-sdk/core';
 import { initializeConfig } from '@ckb-lumos/config-manager';
 
-const sporeConfig: SporeConfig = predefinedSporeConfigs.Mainnet;
+const sporeConfig: SporeConfig = process.env.NODE_ENV === 'development' ?  predefinedSporeConfigs.Testnet : predefinedSporeConfigs.Mainnet;
 
 initializeConfig(sporeConfig.lumos);
 setSporeConfig(sporeConfig);
