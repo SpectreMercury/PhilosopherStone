5189b1697686f7161a88145e7f4dcab7e18782ca
lee920217 <francislee920217@gmail.com>
2024-02-19 23:47:33 +0800

    withdraw function but got capacity error

---
+++ src/app/withdraw/page.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import useWalletBalance from "@/hooks/useBalance";
+import { useConnect } from "@/hooks/useConnect";
+import { RootState } from "@/store/store";
+import { BI } from "@ckb-lumos/bi";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Button from "@/app/_components/Button/Button";
+import { transfer } from "@/utils/withdraw";
+
+const Withdraw: React.FC = () => {
+    const [toAddress, setToAddress] = useState<string>('');
+    const [amount, setAmount] = useState<string>();
+    const { lock, address } = useConnect();
+    const balance = useWalletBalance(address!!);
+
+    const widthDrawFunc = async () => {
+        if (!toAddress || !address || !amount) return
+        transfer({
+            amount: amount,
+            from: address,
+            to: toAddress
+        }).then(res => {
+            console.log(res);
+        })
+    }
+
+    return (
+        <div>
+            <p className=" font-Montserrat text-hd1mb text-white001 text-center mt-12">Withdraw</p>
+            <div className='flex flex-col px-4'>
+                <p className='text-white001 font-SourceSanPro text-body1bdmb mt-4'>Recipient’s wallet address*</p>
+                <input 
+                    id="walletAddress"
+                    placeholder='E.g. 0xAbCdEfGhIjKlMnOpQrStUvWxYz0123456789'
+                    value={toAddress}
+                    onChange={(e) => setToAddress(e.target.value)}
+                    className='w-full h-12 border border-white009 rounded-lg bg-primary008 mt-2 px-4 text-white001' />
+            </div>
+            <div className='flex flex-col px-4'>
+                <p className='text-white001 font-SourceSanPro text-body1bdmb mt-4'>Capacity</p>
+                <input 
+                    id="capacity"
+                    placeholder={`max: ${balance - 1000 } CKB`}
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    className='w-full h-12 border border-white009 rounded-lg bg-primary008 mt-2 px-4 text-white001' />
+            </div>
+            <div className='flex flex-col px-4'>
+                <Button 
+                    type='solid' 
+                    label='Withdraw' 
+                    onClick={widthDrawFunc}
+                    className='px-4 my-8'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Withdraw
---
+++ src/utils/withdraw.ts
@@ -0,0 +1,146 @@
+import { BI, Cell, helpers, Indexer, RPC, config, commons } from "@ckb-lumos/lumos";
+import { blockchain, bytify, hexify } from "@ckb-lumos/lumos/codec";
+import { Script } from '../gql/graphql';
+import { sporeConfig } from "./config";
+
+const CKB_RPC_URL = sporeConfig.ckbNodeUrl;
+const rpc = new RPC(CKB_RPC_URL);
+const indexer = new Indexer(CKB_RPC_URL);
+
+interface Options {
+  from: string;
+  to: string;
+  amount: string;
+}
+
+// prettier-ignore
+interface EthereumRpc {
+    (payload: { method: 'personal_sign'; params: [string /*from*/, string /*message*/] }): Promise<string>;
+}
+
+const SECP_SIGNATURE_PLACEHOLDER = hexify(
+  new Uint8Array(
+    commons.omnilock.OmnilockWitnessLock.pack({
+      signature: new Uint8Array(65).buffer,
+    }).byteLength
+  )
+);
+
+// prettier-ignore
+export interface EthereumProvider {
+    selectedAddress: string;
+    isMetaMask?: boolean;
+    enable: () => Promise<string[]>;
+    addListener: (event: 'accountsChanged', listener: (addresses: string[]) => void) => void;
+    removeEventListener: (event: 'accountsChanged', listener: (addresses: string[]) => void) => void;
+    request: EthereumRpc;
+  }
+// @ts-ignore
+export const ethereum = window.ethereum as EthereumProvider;
+
+export async function transfer(options: Options): Promise<string> {
+  let tx = helpers.TransactionSkeleton({});
+  const fromScript = helpers.parseAddress(options.from);
+  const toScript = helpers.parseAddress(options.to);
+
+  // additional 0.001 ckb for tx fee
+  // the tx fee could calculated by tx size
+  // this is just a simple example
+  const neededCapacity = BI.from(options.amount).add(100000);
+  let collectedSum = BI.from(0);
+  const collectedCells: Cell[] = [];
+  const collector = indexer.collector({ lock: fromScript, type: "empty" });
+  for await (const cell of collector.collect()) {
+    collectedSum = collectedSum.add(cell.cellOutput.capacity);
+    collectedCells.push(cell);
+    if (BI.from(collectedSum).gte(neededCapacity)) break;
+  }
+
+  if (collectedSum.lt(neededCapacity)) {
+    throw new Error(`Not enough CKB, expected: ${neededCapacity}, actual: ${collectedSum} `);
+  }
+
+  const transferOutput: Cell = {
+    cellOutput: {
+      capacity: BI.from(options.amount).toHexString(),
+      lock: toScript,
+    },
+    data: "0x",
+  };
+
+  const changeOutput: Cell = {
+    cellOutput: {
+      capacity: collectedSum.sub(neededCapacity).toHexString(),
+      lock: fromScript,
+    },
+    data: "0x",
+  };
+
+  tx = tx.update("inputs", (inputs) => inputs.push(...collectedCells));
+  tx = tx.update("outputs", (outputs) => outputs.push(transferOutput, changeOutput));
+  tx = tx.update("cellDeps", (cellDeps) =>
+    cellDeps.push(
+      {
+        outPoint: {
+          txHash: sporeConfig.lumos.SCRIPTS.OMNILOCK?.TX_HASH!!, 
+          index: sporeConfig.lumos.SCRIPTS.OMNILOCK?.INDEX!!,
+        },
+        depType: sporeConfig.lumos.SCRIPTS.OMNILOCK?.DEP_TYPE!!,
+      },
+      // SECP256K1 lock is depended by omni lock
+      {
+        outPoint: {
+          txHash: sporeConfig.lumos.SCRIPTS.SECP256K1_BLAKE160!!.TX_HASH,
+          index: sporeConfig.lumos.SCRIPTS.SECP256K1_BLAKE160!!.INDEX,
+        },
+        depType: sporeConfig.lumos.SCRIPTS.SECP256K1_BLAKE160!!.DEP_TYPE,
+      }
+    )
+  );
+
+  const witness = hexify(blockchain.WitnessArgs.pack({ lock: SECP_SIGNATURE_PLACEHOLDER }));
+
+  // fill txSkeleton's witness with placeholder
+  for (let i = 0; i < tx.inputs.toArray().length; i++) {
+    tx = tx.update("witnesses", (witnesses) => witnesses.push(witness));
+  }
+
+  tx = commons.omnilock.prepareSigningEntries(tx, { config: sporeConfig.lumos });
+
+  let signedMessage = await ethereum.request({
+    method: "personal_sign",
+    params: [ethereum.selectedAddress, tx.signingEntries.get(0)!!.message],
+  });
+
+  let v = Number.parseInt(signedMessage.slice(-2), 16);
+  if (v >= 27) v -= 27;
+  signedMessage = "0x" + signedMessage.slice(2, -2) + v.toString(16).padStart(2, "0");
+
+  const signedWitness = hexify(
+    blockchain.WitnessArgs.pack({
+      lock: commons.omnilock.OmnilockWitnessLock.pack({
+        signature: bytify(signedMessage).buffer,
+      }),
+    })
+  );
+
+  tx = tx.update("witnesses", (witnesses) => witnesses.set(0, signedWitness));
+
+  const signedTx = helpers.createTransactionFromSkeleton(tx);
+  const txHash = await rpc.sendTransaction(signedTx, "passthrough");
+
+  return txHash;
+}
+
+export async function capacityOf(address: string): Promise<BI> {
+  const collector = indexer.collector({
+    lock: helpers.parseAddress(address),
+  });
+
+  let balance = BI.from(0);
+  for await (const cell of collector.collect()) {
+    balance = balance.add(cell.cellOutput.capacity);
+  }
+
+  return balance;
+}
