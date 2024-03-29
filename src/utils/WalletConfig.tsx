df80cb6a94d84930e604aef2d4d782831b7919ce
lee920217 <francislee920217@gmail.com>
2024-03-29 10:50:42 +0800

    add joyid wallet support

--- src/utils/WalletConfig.tsx
+++ src/utils/WalletConfig.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image';
 import { WalletConfig } from "@/types/Wallet";
 
 let WalletConfigData: WalletConfig[] = [
-  // {
-  //   name: 'Metamask',
-  //   recommended: false,
-  //   logo: <Image src='/svg/metamask-icon.svg' alt='Metamask' width={24} height={24} />,
-  // },
+  {
+    name: 'JoyID',
+    recommended: false,
+    logo: <Image src='/svg/joyid-icon.svg' alt='JoyID' width={24} height={24} />,
+  }
 ];
 
-WalletConfigData = process.env.NODE_ENV === 'development' ? [...WalletConfigData, {
-  name: 'JoyID',
-  recommended: false,
-  logo: <Image src='/svg/joyid-icon.svg' alt='JoyID' width={24} height={24} />,
-},] : WalletConfigData
+// WalletConfigData = process.env.NODE_ENV === 'development' ? [...WalletConfigData, {
+//   name: 'JoyID',
+//   recommended: false,
+//   logo: <Image src='/svg/joyid-icon.svg' alt='JoyID' width={24} height={24} />,
+// },] : WalletConfigData
 
 
 export default WalletConfigData;
